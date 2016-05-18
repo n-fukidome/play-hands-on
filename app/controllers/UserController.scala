667d7aea47fea66e73e78a67afa3cdabc22766fd
n-fukidome <n-fukidome@nekojarashi.com>
2016-05-18 15:46:39 +0900

    UPDATE & CREATE
    - ユーザ登録・編集画面の実装
    - 登録、更新処理の実装

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -27,24 +27,98 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
     }
   }
 
+  // コンパニオンオブジェクトに定義したFormを参照するためにimport文を追加
+  import UserController._
+
   /**
    * 編集画面表示
    */
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]) = Action.async { implicit rs =>
+    // リクエストパラメータにIDが存在する場合
+    val form = if(id.isDefined) {
+      // IDからユーザ情報を1件取得
+      db.run(Users.filter(t => t.id === id.get.bind).result.head).map { user =>
+        // 値をフォームに詰める
+        userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+      }
+    } else {
+      // リクエストパラメータにIDが存在しない場合
+      Future { userForm }
+    }
+
+    form.flatMap { form =>
+      // 会社一覧を取得
+      db.run(Companies.sortBy(_.id).result).map { companies =>
+        Ok(views.html.user.edit(form, companies))
+      }
+    }
+  }
 
   /**
    * 登録実行
    */
-  def create = TODO
+  def create = Action.async { implicit rs =>
+    // リクエストの内容をバインド
+    userForm.bindFromRequest.fold(
+      // エラーの場合
+      error => {
+        db.run(Companies.sortBy(t => t.id).result).map { companies =>
+          BadRequest(views.html.user.edit(error, companies))
+        }
+      },
+      // OKの場合
+      form  => {
+        // ユーザを登録
+        val user = UsersRow(0, form.name, form.companyId)
+        db.run(Users += user).map { _ =>
+          // 一覧画面へリダイレクト
+          Redirect(routes.UserController.list)
+        }
+      }
+    )
+  }
 
   /**
    * 更新実行
    */
-  def update = TODO
+  def update = Action.async { implicit rs =>
+    // リクエストの内容をバインド
+    userForm.bindFromRequest.fold(
+      // エラーの場合は登録画面に戻す
+      error => {
+        db.run(Companies.sortBy(t => t.id).result).map { companies =>
+          BadRequest(views.html.user.edit(error, companies))
+        }
+      },
+      // OKの場合は登録を行い一覧画面にリダイレクトする
+      form  => {
+        // ユーザ情報を更新
+        val user = UsersRow(form.id.get, form.name, form.companyId)
+        db.run(Users.filter(t => t.id === user.id.bind).update(user)).map { _ =>
+          // 一覧画面にリダイレクト
+          Redirect(routes.UserController.list)
+        }
+      }
+    )
+  }
 
   /**
    * 削除実行
    */
   def remove(id: Long) = TODO
 
+}
+
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> optional(longNumber),
+      "name"      -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
 }
