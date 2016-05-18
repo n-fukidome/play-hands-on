9f0209f4c83b35b3e37840a074b5b3def83468ab
n-fukidome <n-fukidome@nekojarashi.com>
2016-05-18 14:57:06 +0900

    my first commit
    プロジェクトの作成〜ユーザ一覧の実装

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,50 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
+                               val messagesApi: MessagesApi) extends Controller
+    with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+
+  /**
+   * 一覧表示
+   */
+  def list = Action.async { implicit rs =>
+    // IDの昇順にすべてのユーザ情報を取得
+    db.run(Users.sortBy(t => t.id).result).map { users =>
+      // 一覧画面を表示
+      Ok(views.html.user.list(users))
+    }
+  }
+
+  /**
+   * 編集画面表示
+   */
+  def edit(id: Option[Long]) = TODO
+
+  /**
+   * 登録実行
+   */
+  def create = TODO
+
+  /**
+   * 更新実行
+   */
+  def update = TODO
+
+  /**
+   * 削除実行
+   */
+  def remove(id: Long) = TODO
+
+}
---
+++ build.sbt
@@ -0,0 +1,20 @@
+name := """play2-hands-on"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  // jdbc,
+  // cache,
+  // ws,
+  // "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
+  "com.h2database" % "h2" % "1.4.177",
+  "com.typesafe.play" %% "play-slick" % "1.0.0"
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+routesGenerator := InjectedRoutesGenerator
